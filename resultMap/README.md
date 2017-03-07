170ef057ae9e6bdb34adbdfdcfac2f5f33326c12
loserStar <362527240@qq.com>
2017-03-07 09:59:30 +0800

    resultMap,输出结果集映射

---
+++ resultMap/README.md
@@ -0,0 +1,4 @@
+#mybatis的示例程序-基于mybatis，mapper文件中使用resultMap来做输出结果集的映射，用在查询到的数据与实体类的字段名不对应的情况
+
+##使用gradle和maven两种构建方式
+
---
+++ resultMap/src/main/java/dao/UserDao.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.util.List;
+
+import entity.User;
+import entity.UserVo;
+
+public interface UserDao {
+	
+	/**
+	 * 根据id得到一个user
+	 * @param i
+	 * @return
+	 */
+	public User get(int i);
+	
+	
+	/**
+	 * 根据条件查询用户
+	 * @param user
+	 * @return
+	 */
+	public List<User> findList(User user);
+	
+	/**
+	 * 插入一个新的user记录
+	 * @param user
+	 */
+	public void insert(User user);
+	
+	/**
+	 * 根据id修改一个user
+	 * @param user
+	 */
+	public void update(User user) ;
+	
+	/**
+	 * 根据id删除一个user
+	 * @param i
+	 */
+	public void delete(int i) ;
+	
+	/**
+	 * 使用映射结果集
+	 * @param user
+	 * @return
+	 */
+	public List<UserVo> findUserVoList(User user);
+		
+}
